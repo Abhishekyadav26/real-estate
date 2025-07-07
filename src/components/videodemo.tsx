8e8079aaf2b2f10e109b197e09b30c83718a5315
Abhishek kumar yadav <143870083+Abhishekyadav26@users.noreply.github.com>
2025-07-08 04:09:34 +0530

    Update videodemo.tsx

--- src/components/videodemo.tsx
+++ src/components/videodemo.tsx
@@ -6,14 +6,14 @@ export function HeroVideo() {
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/5Q5EP87-a0U"
+        videoSrc="https://www.youtube.com/embed/C6juoFtEz8w"
         thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/5Q5EP87-a0U"
+        videoSrc="https://www.youtube.com/embed/C6juoFtEz8w"
         thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
         thumbnailAlt="Hero Video"
       />
